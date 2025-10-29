dd3850ef5c65a2ecb8c0db81344b5354b2b8a00c
jesus <jesus@2806-104e-0013-73f2-0000-0000-0000-0007.ipv6.infinitum.net.mx>
2025-10-28 18:03:34 -0600

    formuklario videop 2

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,39 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule, FormArray, Form } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './dynamic-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+
+  private fb = inject(FormBuilder)
+  formUtils = FormUtils
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['mario bros', Validators.required],
+        ['Death stranding', Validators.required]
+      ],
+      Validators.minLength(3)
+    )
+  })
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  //errores
+  isValidFiledInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    )
+  }
+
+
+}
