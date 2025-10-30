f5a29635258c99f90c1edd3a4188981b9cbf9335
jesus <jesus@2806-104e-0013-73f2-0000-0000-0000-0007.ipv6.infinitum.net.mx>
2025-10-29 18:00:19 -0600

    trabajo inicial de los switches

--- src/app/reactive/pages/switches/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches/switches-page/switches-page.component.ts
@@ -1,22 +1,32 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe, NgClass } from '@angular/common';
+import { FormUtils } from '../../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule, NgClass],
   templateUrl: './switches-page.component.html',
 })
 export class SwitchesPageComponent {
 
   private fb = inject(FormBuilder)
+  formUtils = FormUtils
   //form builder
-  myForm = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0]
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    WantNotifications: [true],
+    termAndConditions: [false, Validators.requiredTrue]
 
   })
 
+
+  onSubmit() {
+    this.myForm.markAllAsTouched()
+    console.log('====================================');
+    console.log(this.myForm.value);
+    console.log('====================================');
+  }
 
 
   // la otra forma compleja de hacerlo
